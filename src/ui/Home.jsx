293f9eaaaa013a142f0f8a411cfa01a214e6a2f3
MrJohnAlex <judi699095@gmail.com>
2024-12-26 17:06:23 +0630

    first commit

---
+++ src/ui/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Pizza App</h1>
+      <p>This is a starter template for creating a basic Next.js app.</p>
+    </div>
+  );
+}
